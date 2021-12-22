a3f87843e56e0206e2dcc623ac91bcabdf4ca4cd
LeeGeonJae <wnwkrdkemf@gmail.com>
2021-12-22 17:47:04 +0900

    반복문 (1 - 100까지 합과 평균, 100까지 짝수의 합)

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
 {
 	//1 + 2 + 3 + 4 + ... + 100 을 반복문으로 구해보시오
 	//array배열
+
 	int Korean[100] = { 0, };	// { 0, } 은 0 이후의 모든 값을 0으로 저장
 
 	for (int i = 0; i < 100; ++i)	// for( 초기치; 조건; 증가치 ) ( ++i = 1 + i )
@@ -19,17 +20,28 @@ int main()
 	//1 - 100까지 합과 평균을 구하세요.
 	//Sum = Sum + 1;
 	//Sum = Sum + 2;
+
 	int Sum = 0;
 	float Avg = 0.0f;
 
-		for (int i = 1; i <= 100; i = i++)
+	for (int i = 1; i <= 100; i = i++)
 	{
 		Sum = Sum + i;
 		cout << Sum << ", ";
 	}
-		cout << endl;
-		Avg = static_cast<float>(Sum) / 100.0f;
-		cout << Avg << endl;
+	cout << endl;
+	Avg = static_cast<float>(Sum) / 100.0f;
+	cout << Avg << endl;
 
+
+	//100까지 짝수의 합을 구하시오.
+
+	int EvenNumber = 0;
+
+	for (int i = 2; i <= 100; i = i + 2)
+		{
+			EvenNumber = EvenNumber + i;
+			cout << EvenNumber << ", ";
+		}
 	return 0;
 }
